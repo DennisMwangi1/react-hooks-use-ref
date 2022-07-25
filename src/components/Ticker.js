37977b50cd39fc7adf4f4e67287ba24b6252f27a
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-07-25 08:58:30 +0300

    Done.

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,16 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
   const [color, setColor] = useState("black");
+  const prevPriceRef = useRef(price)
 
   useEffect(() => {
-    const id = setInterval(() => setPrice(makeRandomNumber), 1000);
+    const id = setInterval(() =>{ 
+      
+      setPrice(makeRandomNumber)
+    }, 1000);
     return function () {
       clearInterval(id);
     };
   }, []);
+  
+  useEffect(()=>{
+    const prevPrice = prevPriceRef.current
+if(price>prevPrice){
+  setColor('green');
+}
+else if(price<prevPrice){
+  setColor('red')
+}else{
+  setColor('black')
+}
+  },[price])
 
   return (
     <div>
